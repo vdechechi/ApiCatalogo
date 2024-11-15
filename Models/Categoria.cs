6c4579c5e9be224864377e0f84caea5d4e2d654d
Vinicius <vinicius.gdechechi@gmail.com>
2024-11-15 12:30:35 -0300

    models criados

---
+++ Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿namespace ApiCatalogo.Models
+{
+    public class Categoria
+    {
+        public Categoria()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+        public string? Nome { get; set; } = String.Empty;
+        public string? ImagemUrl { get; set; } = String.Empty;
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Produto
+{
+    public Produto()
+    {
+        Id = Guid.NewGuid();
+        DataCadastro = DateTime.UtcNow;
+    }
+    public Guid Id { get; set; }
+    public string Nome { get; set; } = String.Empty;
+    public string Descricao { get; set; } = String.Empty;
+    public string ImagemUrl { get; set; } = String.Empty;
+    public decimal Preco { get; set; }
+    public decimal Estoque { get; set; }
+    public DateTime DataCadastro { get; set; }
+}
